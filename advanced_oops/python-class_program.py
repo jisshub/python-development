f859e06169882b9197bf56ed8376ef74d3850ecc
jissmon jose <jissmon476@gmail.com>
2020-03-16 21:45:28 +0530

    class program modified

--- advanced_oops/python-class_program.py
+++ advanced_oops/python-class_program.py
@@ -39,6 +39,7 @@ class FootballTeam(SportsClub):
         super().__init__(founded, city)
         self.team_size = None
         self.team_manager = None
+        self.players_list = []
 
     # define a private method -1
     def __add_team_info(self):
@@ -47,15 +48,14 @@ class FootballTeam(SportsClub):
 
     # private method -2
     def __add_players(self):
-        players_list = []
         while self.team_size > 0:
             player_name = input("Name: ")
             shirt_number = int(input("Shirt Number: "))
             player_role = input("Player Role: ")
             # append all to a list in form dict
-            players_list.append(dict(playerName=player_name, shirtNumber=shirt_number,
-                                     playerRole=player_role))
-        self.team_size += 1
+            self.players_list.append(dict(playerName=player_name, shirtNumber=shirt_number,
+                                          playerRole=player_role))
+            self.team_size -= 1
 
     # access the private method from public methods
     def get_team_info(self):
@@ -64,10 +64,17 @@ class FootballTeam(SportsClub):
         return f"{self.team_size}, {self.team_manager}"
 
     def get_players(self):
+        # access the private method __add_players
         self.__add_players()
+        for each_player in self.players_list:
+            for pl_key, pl_value in each_player.items():
+                print(pl_key, pl_value)
+            break
 
 
-club1 = SportsClub(1917, "Madrid")
+club1 = FootballTeam(1917, "Madrid")
 club1.set_president_revenue()
 print(club1.get_president_revenue())
 club1.diff_sports_type()
+print(club1.get_team_info())
+club1.get_players()
