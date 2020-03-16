4c4c58e7e1450eafe15148ce3d765edf09c05fff
jisshub <jissmon476@gmail.com>
2020-03-16 13:37:37 +0530

    python class program first commit

---
+++ advanced_oops/python-class_program.py
@@ -0,0 +1,73 @@
+# sample program using python class
+
+
+class SportsClub:
+    clubName = "Real Madrid"
+
+    def __init__(self, founded, city):
+        # instance variables
+        self.city = city
+        self.founded = founded
+        self.__president = None
+        self.__totalRevenue = None
+
+    # set the value
+    def set_president_revenue(self):
+        self.__president = input("President: ")
+        self.__totalRevenue = input("Revnue($): ")
+
+    # get those value
+    def get_president_revenue(self):
+        return f"{SportsClub.clubName}'s president is" \
+               f"{self.__president} and has revenue of {self.__totalRevenue}"
+
+    def diff_sports_type(self):
+        sports_list = list()
+        while len(sports_list) < 3:
+            games = input("sports: ")
+            sports_list.append(games)
+        print(f"{SportsClub.clubName} has {len(sports_list)} teams")
+        for counter, games in enumerate(sports_list):
+            print(f"{counter}: {games}")
+
+
+class FootballTeam(SportsClub):
+    teamName = "Real Madrid CF"
+    stadium = "santiago bernabue"
+
+    def __init__(self, founded, city):
+        super().__init__(founded, city)
+        self.team_size = None
+        self.team_manager = None
+
+    # define a private method -1
+    def __add_team_info(self):
+        self.team_size = int(input("squad size: "))
+        self.team_manager = input("manager: ")
+
+    # private method -2
+    def __add_players(self):
+        players_list = []
+        while self.team_size > 0:
+            player_name = input("Name: ")
+            shirt_number = int(input("Shirt Number: "))
+            player_role = input("Player Role: ")
+            # append all to a list in form dict
+            players_list.append(dict(playerName=player_name, shirtNumber=shirt_number,
+                                     playerRole=player_role))
+        self.team_size += 1
+
+    # access the private method from public methods
+    def get_team_info(self):
+        # gets private method-1
+        self.__add_team_info()
+        return f"{self.team_size}, {self.team_manager}"
+
+    def get_players(self):
+        self.__add_players()
+
+
+club1 = SportsClub(1917, "Madrid")
+club1.set_president_revenue()
+print(club1.get_president_revenue())
+club1.diff_sports_type()
