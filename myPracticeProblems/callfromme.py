c4b60ed4c0ae8d6b641f92d9d9c1bf0a8573f6ae
jisshub <jissmon476@gmail.com>
2020-03-19 12:59:25 +0530

    dummy_data_csv_json.py completed - callfromme.py

---
+++ myPracticeProblems/callfromme.py
@@ -0,0 +1,8 @@
+# accessing dummy_data_csv_json module
+
+import dummy_data_csv_json
+
+new = dummy_data_csv_json.DummyClass
+obj = new()
+obj.need_all_infos()
+obj.store_to_json()
--- myPracticeProblems/dummy_data_csv_json.py
+++ myPracticeProblems/dummy_data_csv_json.py
@@ -10,51 +10,63 @@ names_2 = ["kumar", "jose", "george", "vinay", "sourav", "kroos",
 place_list = ["angamaly", "kochi", "thrissur", 'kakkanad',
               "thevara", "kalady", 'seattle']
 
+
 # storing in a csv file
-with open("./extrafiles/person.csv", "w") as csv_file:
-    write_to_csv = writer(csv_file)
-    write_to_csv.writerow(["first Name".upper(), "last name".upper(),
-                           "ssn".upper(), "age".upper(), "place".upper()])
+# with open("./extrafiles/person.csv", "w") as csv_file:
+#     write_to_csv = writer(csv_file)
+#     write_to_csv.writerow(["first Name".upper(), "last name".upper(),
+#                            "ssn".upper(), "age".upper(), "place".upper()])
+#
 
-    def need_all_infos():
+class DummyClass:
+    def __init__(self):
+        # generate some dummy data
+        self.first_name = random.choice(names_1)
+        self.last_name = random.choice(names_2)
+        self.ssn = random.randint(4242342342, 7897978987)
+        self.age = random.randint(30, 51)
+        self.place = random.choice(place_list)
+
+    def need_all_infos(self):
+        # storing in a csv file
+        with open("./extrafiles/person.csv", "w") as csv_file:
+            write_to_csv = writer(csv_file)
+            write_to_csv.writerow(["first Name".upper(), "last name".upper(),
+                                   "ssn".upper(), "age".upper(), "place".upper()])
+
+            for count in range(10):
+                # create a csv file and write data  to it.
+                write_to_csv.writerow([self.first_name,
+                                       self.last_name,
+                                       self.ssn,
+                                       self.age,
+                                       self.place])
+
+    # storing in json file
+    def store_to_json(self):
+        data_list = []
+        # newDict = {}
         for count in range(10):
-            # generate some dummy data
-            first_name = random.choice(names_1)
-            last_name = random.choice(names_2)
-            ssn = random.randint(4242342342, 7897978987)
-            age = random.randint(30, 51)
-            place = random.choice(place_list)
-            #       create a csv file and write data  to it.
-            write_to_csv.writerow([first_name, last_name, ssn, age, place])
+            # create a dictionary and add
+            data_dict = dict(firstName=self.first_name,
+                             lastName=self.last_name,
+                             ssn=self.ssn,
+                             Age=self.age,
+                             Place=self.place)
+            # append dict item in a list
+            data_list.append(data_dict)
+        # store into json file
+        with open('./extrafiles/personData.json', "w") as pwr:
+            json.dump(data_list, pwr)
+        # read from a json file
+        with open("./extrafiles/personData.json", "r") as pwr:
+            json_contents = json.load(pwr)
+            for all_data in json_contents:
+                print('\n')
+                print(all_data)
 
 
-    need_all_infos()
-
-
-# storing in json file
-def store_to_json():
-    data_list = []
-    # newDict = {}
-    for count in range(10):
-        first_name = random.choice(names_1)
-        last_name = random.choice(names_2)
-        ssn = random.randint(4242342342, 7897978987)
-        age = random.randint(30, 51)
-        place = random.choice(place_list)
-        # create a dictionary and add
-        data_dict = dict(firstName=first_name, lastName=last_name,
-                         ssn=ssn, Age=age, Place=place)
-        # append dict item in a list
-        data_list.append(data_dict)
-    # store into json file
-    with open('./extrafiles/personData.json', "w") as pwr:
-        json.dump(data_list, pwr)
-    # read from a json file
-    with open("./extrafiles/personData.json", "r") as pwr:
-        json_contents = json.load(pwr)
-        for all_data in json_contents:
-            print('\n')
-            print(all_data)
-
-
-store_to_json()
+# create an instance
+fileObj = DummyClass()
+fileObj.need_all_infos()
+fileObj.store_to_json()
